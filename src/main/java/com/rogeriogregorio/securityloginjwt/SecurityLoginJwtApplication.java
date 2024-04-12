07d0edd068b50f02bec650c0dfafce15434b397a
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-04-12 20:35:27 -0300

    feat: Initial commit

---
+++ src/main/java/com/rogeriogregorio/securityloginjwt/SecurityLoginJwtApplication.java
@@ -0,0 +1,13 @@
+package com.rogeriogregorio.securityloginjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLoginJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityLoginJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rogeriogregorio/securityloginjwt/SecurityLoginJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogeriogregorio.securityloginjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLoginJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
