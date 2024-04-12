53211546ad06bf9e7e6c7afb8e5b608f9330d28f
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-04-12 20:57:44 -0300

    feat: Add PrivateController

---
+++ src/main/java/com/rogeriogregorio/securityloginjwt/controllers/PrivateController.java
@@ -0,0 +1,15 @@
+package com.rogeriogregorio.securityloginjwt.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("private")
+public class PrivateController {
+
+    @GetMapping
+    public String getMessage() {
+        return "Hello from private API controller";
+    }
+}
