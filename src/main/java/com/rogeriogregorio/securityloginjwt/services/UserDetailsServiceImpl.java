01d06d3c56ec33c73723113339ee7fa1e9db56a7
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-04-12 23:42:22 -0300

    feat: Add services

---
+++ src/main/java/com/rogeriogregorio/securityloginjwt/services/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.rogeriogregorio.securityloginjwt.services;
+
+import com.rogeriogregorio.securityloginjwt.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(UserAuthenticated::new)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException("User Not Found with username: " + username));
+    }
+}
