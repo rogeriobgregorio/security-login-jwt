c273f2b24487731d182646dd294c7f0c2efa113d
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-12-12 21:04:27 -0300

    refactor: Refactor CustomHealthCheck class

--- src/main/java/com/rogeriogregorio/securityloginjwt/health/CustomHealthCheck.java
+++ src/main/java/com/rogeriogregorio/securityloginjwt/health/CustomHealthCheck.java
@@ -1,5 +1,6 @@
 package com.rogeriogregorio.securityloginjwt.health;
 
+import com.rogeriogregorio.securityloginjwt.security.utils.CatchError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
@@ -7,13 +8,18 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.SQLException;
 
 @Component
 public class CustomHealthCheck implements HealthIndicator {
 
+    private final CatchError catchError;
+    private final DataSource dataSource;
+
     @Autowired
-    private DataSource dataSource;
+    public CustomHealthCheck(CatchError catchError, DataSource dataSource) {
+        this.catchError = catchError;
+        this.dataSource = dataSource;
+    }
 
     @Override
     public Health health() {
@@ -27,17 +33,10 @@ public class CustomHealthCheck implements HealthIndicator {
 
     private boolean checkDatabaseConnection() {
 
-        try (Connection connection = dataSource.getConnection()) {
-
-            if (connection != null && !connection.isClosed()) {
-                return true;
-            }
-
-        } catch (SQLException e) {
-            return false;
-        }
-
-        return false;
+        return catchError.run(() -> {
+            Connection connection = dataSource.getConnection();
+            return connection != null && !connection.isClosed();
+        });
     }
 }
 
