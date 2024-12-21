e4886d3eb2b83660b5e433bd982ed54a75a1fa83
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2024-12-20 23:49:54 -0300

    refactor: Refactor UserController class

--- src/main/java/com/rogeriogregorio/securityloginjwt/security/controllers/UserController.java
+++ src/main/java/com/rogeriogregorio/securityloginjwt/security/controllers/UserController.java
@@ -3,6 +3,12 @@ package com.rogeriogregorio.securityloginjwt.security.controllers;
 import com.rogeriogregorio.securityloginjwt.security.dto.UserRequest;
 import com.rogeriogregorio.securityloginjwt.security.dto.UserResponse;
 import com.rogeriogregorio.securityloginjwt.security.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users", produces = {"application/json"})
+@Tag(name = "Users API", description = "API para gestão de usuários")
 public class UserController {
 
     private final UserService userService;
@@ -23,6 +30,14 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Buscar todos os usuários", description = "Endpoint para buscar todos os usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponse.class))),
+            @ApiResponse(responseCode = "403", description = "Não autorizado"),
+            @ApiResponse(responseCode = "500", description = "Erro ao buscar usuários")
+    })
     @GetMapping
     public ResponseEntity<List<UserResponse>> getAllUsers(Pageable pageable) {
 
@@ -31,6 +46,14 @@ public class UserController {
                 .body(userService.getAllUsers(pageable).getContent());
     }
 
+    @Operation(summary = "Criar usuário", description = "Endpoint para criar usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Criação realizada com sucesso",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Usuário já criado"),
+            @ApiResponse(responseCode = "500", description = "Erro ao tentar criar")
+    })
     @PostMapping
     public ResponseEntity<UserResponse> createUser(
             @Valid @RequestBody UserRequest userRequest) {
@@ -40,6 +63,15 @@ public class UserController {
                 .body(userService.createUser(userRequest));
     }
 
+    @Operation(summary = "Atualizar papel (role) do usuário", description = "Endpoint para atualizar papel (role) do usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Papel (role) atualizada com sucesso",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponse.class))),
+            @ApiResponse(responseCode = "403", description = "Não autorizado"),
+            @ApiResponse(responseCode = "404", description = "Nenhum usuário encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro ao atualizar papel (role) do usuário")
+    })
     @PatchMapping(value = "/roles/{id}")
     public ResponseEntity<UserResponse> updateUserRole(
             @PathVariable String id,
@@ -50,6 +82,15 @@ public class UserController {
                 .body(userService.updateUserRole(id, userRole));
     }
 
+    @Operation(summary = "Buscar usuário por Id",description = "Endpoint para buscar usuário pelo Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponse.class))),
+            @ApiResponse(responseCode = "403", description = "Não autorizado"),
+            @ApiResponse(responseCode = "404", description = "Nenhum usuário encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro ao buscar usuário")
+    })
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserResponse> getUserById(@PathVariable String id) {
 
@@ -58,6 +99,15 @@ public class UserController {
                 .body(userService.getUserById(id));
     }
 
+    @Operation(summary = "Atualizar usuário", description = "Endpoint para atualizar usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "usuário atualizado com sucesso",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponse.class))),
+            @ApiResponse(responseCode = "403", description = "Não autorizado"),
+            @ApiResponse(responseCode = "404", description = "Nenhum usuário encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro ao atualizar usuário")
+    })
     @PutMapping(value = "/{id}")
     public ResponseEntity<UserResponse> updateUser(
             @PathVariable String id,
@@ -68,6 +118,13 @@ public class UserController {
                 .body(userService.updateUser(id, userRequest));
     }
 
+    @Operation(summary = "Deletar usuário", description = "Endpoint para deletar usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário deletado com sucesso"),
+            @ApiResponse(responseCode = "403", description = "Não autorizado"),
+            @ApiResponse(responseCode = "404", description = "Nenhum usuário encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro ao deletar usuário")
+    })
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable String id) {
 
@@ -78,6 +135,15 @@ public class UserController {
                 .build();
     }
 
+    @Operation(summary = "Buscar usuário por email",
+            description = "Endpoint para buscar usuários por email")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponse.class))),
+            @ApiResponse(responseCode = "403", description = "Não autorizado"),
+            @ApiResponse(responseCode = "500", description = "Erro ao buscar usuário")
+    })
     @GetMapping(value = "/search/email")
     public ResponseEntity<UserResponse> getUserByEmail(
             @RequestParam String email) {
